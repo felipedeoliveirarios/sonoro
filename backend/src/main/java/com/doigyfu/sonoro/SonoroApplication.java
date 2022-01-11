b45ff5016805de2e8a62e0400f9aff76f0b93216
Felipe Rios <felipe.rios@cilia.com.br>
2022-01-11 14:31:17 -0300

    Project Created

---
+++ backend/src/main/java/com/doigyfu/sonoro/SonoroApplication.java
@@ -0,0 +1,13 @@
+package com.doigyfu.sonoro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SonoroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SonoroApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/doigyfu/sonoro/SonoroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.doigyfu.sonoro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SonoroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
